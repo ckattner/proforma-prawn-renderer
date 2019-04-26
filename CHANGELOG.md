d47c84a59b8ef855fec65ab209d03de7e434bdf2
Matthew Ruggio <mruggio@bluemarblepayroll.com>
2019-04-26 13:52:55 -0500

    Bumped version for 1.0.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.0.0 (April 26th, 2019)
+
+Initial Release.
+
 # 0.0.1 (April 17th, 2019)
 
 Library Shell.
--- lib/proforma/prawn_renderer/version.rb
+++ lib/proforma/prawn_renderer/version.rb
@@ -9,6 +9,6 @@
 
 module Proforma
   class PrawnRenderer
-    VERSION = '1.0.0-alpha.2'
+    VERSION = '1.0.0'
   end
 end
